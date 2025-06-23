d19403a469031907bf5d02421af3ddda5b74f73e
NOCOR <vitabrevislongo@gmail.com>
2025-06-23 17:50:21 +0300

    Update app.js
    Меняем порт

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const _ = require('underscore');
 
-var port = process.env.PORT || 8080;
-var animals = {
-    "cat": "meow",
-    "dog": "bark",
-    "eel": "hiss",
-    "bear": "growl",
-    "frog": "croak",
-    "lion": "roar"
-}
+const port = process.env.PORT || 8080;
+const animals = {
+  "cat": "meow",
+  "dog": "bark",
+  "eel": "hiss",
+  "bear": "growl",
+  "frog": "croak",
+  "lion": "roar"
+};
 
 function getAnimal() {
-  return animal = _.sample(Object.entries(animals));
+  return _.sample(Object.entries(animals));
 }
 
 const app = express();
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   const [animal_name, sound] = getAnimal();
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(`George Orwell had a farm.<br />
@@ -28,15 +28,19 @@ With a ${ sound }-${ sound } here.<br />
 And a ${ sound }-${ sound } there.<br />
 Here a ${ sound }, there a ${ sound }.<br />
 Everywhere a ${ sound }-${ sound }.<br />`);
-      res.end();
+  res.end();
 });
 
-app.get('/api', function(req, res){
+app.get('/api', function(req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(animals));
   res.end();
-})
-
-module.exports =  app.listen(port, () => {
-  console.log(`Launching server on http://localhost:${ port }`)
 });
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Launching server on http://localhost:${ port }`);
+  });
+}
+
+module.exports = app;
